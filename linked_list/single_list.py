087e787737fa1e3cd1fb4245a3e0da1ae023c7eb
Satheesh Mannath <smannath@caremore.com>
2018-12-24 07:43:00 -0500

    linked list operations

--- linked_list/single_list.py
+++ linked_list/single_list.py
@@ -20,6 +20,16 @@ class LinkedList:
             print temp.data
             temp = temp.next
 
+    def insert_at_front(self,new):
+        new.next = self.head
+        self.head = new
+
+    def insert_at_end(self,new):
+        temp = self.head
+        while temp and temp.next!=None:
+            temp = temp.next
+        temp.next = new
+
 llist = LinkedList()
 first = Node(1) #first node
 llist.head = first
@@ -27,4 +37,21 @@ second = Node(2) #second node
 first.next = second
 third = Node(3)
 second.next = third
+fourth = Node(4)
+third.next = fourth
+fifth = Node(5)
+fourth.next = fifth
+sixth = Node(6)
+fifth.next = sixth
+print ('***********Traverse*******************')
+llist.traverse()
+print ('***************Insert at the front *************')
+fourth = Node(0)
+llist.insert_at_front(fourth)
+print ('traverse after insert at front')
+llist.traverse()
+print ('***************Insert at the end *************')
+seven = Node(7)
+llist.insert_at_end(seven)
+print ('traverse after insert at end')
 llist.traverse()
